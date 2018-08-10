ec1d33dfc59a0e4de7212cd8f99268be5fa83566
Prabin Lama <prabin_lama33@hotmail.com>
2018-08-10 13:20:34 +0300

    radio-button, checkbox and select list implemented

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '../../../node_modules/@angular/forms';
-
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "../../../node_modules/@angular/forms";
+import { Department } from "../models/department.model";
 @Component({
-  selector: 'app-create-employee',
-  templateUrl: './create-employee.component.html',
-  styleUrls: ['./create-employee.component.css']
+  selector: "app-create-employee",
+  templateUrl: "./create-employee.component.html",
+  styleUrls: ["./create-employee.component.css"]
 })
 export class CreateEmployeeComponent implements OnInit {
+  // to make the radio button checked by default with the corresponding value
+  //gender= 'male';
+  departments: Department[] = [
+    { id: 1, name: "Help Desk" },
+    { id: 2, name: "IT" },
+    { id: 3, name: "HR" },
+    { id: 4, name: "Administration" }
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  saveEmployee(employeeForm: NgForm): void {
+    console.log(employeeForm.value);
   }
-saveEmployee(employeeForm:NgForm): void{
-  console.log(employeeForm.value);
-}
 }
